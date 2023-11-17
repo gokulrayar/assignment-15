a57f2f61ad93108878aac44a6e45129257affef5
gokulrayar <gokulrayar>
2023-11-17 17:33:18 +0530

    assignment14

---
+++ assignment 15/Binary search/Binary search/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary_search
+{
+    internal class Program
+    {
+        
+            public class BinarySearch
+        {
+            public static int Search(int[] array, int target)
+            {
+                int low = 0;
+                int high = array.Length - 1;
+
+                while (low <= high)
+                {
+                    int mid = low + (high - low) / 2;
+
+                    if (array[mid] == target)
+                    {
+                        return mid;
+                    }
+                    else if (array[mid] < target)
+                    {
+                        low = mid + 1;
+                    }
+                    else
+                    {
+                        high = mid - 1;
+                    }
+                }
+
+                return -1;
+            }
+
+            public static void Main(string[] args)
+            {
+                int[] array = { 2, 4, 5, 1, 3 };
+                int target = 4;
+
+                int index = Search(array, target);
+
+                if (index != -1)
+                {
+                    Console.WriteLine("Element found at index: " + index);
+                }
+                else
+                {
+                    Console.WriteLine("Element not found in the array.");
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+
+}
---
+++ assignment 15/bubble sort/bubble sort/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bubble_sort
+{
+    internal class Program
+    {
+
+            public class BubbleSort
+        {
+            public static void Sort(int[] array)
+            {
+                bool isSwapped;
+
+                do
+                {
+                    isSwapped = false;
+
+                    for (int i = 0; i < array.Length - 1; i++)
+                    {
+                        if (array[i] < array[i + 1])
+                        {
+                            int temp = array[i];
+                            array[i] = array[i + 1];
+                            array[i + 1] = temp;
+                            isSwapped = true;
+                        }
+                    }
+                } while (isSwapped);
+            }
+
+            public static void Main(string[] args)
+            {
+                int[] array = { 5, 2, 4, 1, 3 };
+
+                Console.WriteLine("Unsorted array:");
+                foreach (int element in array)
+                {
+                    Console.Write(element + " ");
+                }
+
+                Sort(array);
+
+                Console.WriteLine("\nSorted array:");
+                foreach (int element in array)
+                {
+                    Console.Write(element + " ");
+                }
+                Console.ReadKey();
+            }
+        }
+
+    }
+}
+    
